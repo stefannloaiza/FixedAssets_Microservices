e32753713327f5ce87cf1ea252e46d7c12d4d1ef
Estefann Loaiza <stefannloa@gmail.com>
2024-02-19 16:16:56 -0500

    Created new eureka server microservice

---
+++ eureka-server/src/main/java/com/perficient/fixedassets/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.fixedassets.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
